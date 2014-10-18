b7b12e6f369bccba02d2b1bee2d97bd43cd997b3
Kai Bepperling <kai.bepperling@gmail.com>
2014-10-18 19:34:28 +0200

    added scripts

---
+++ led_on.rb
@@ -0,0 +1,3 @@
+require 'net/http'
+# Use Net::HTTP to fetch some html
+Net::HTTP.get(URI.parse('http://192.168.2.101/lib/powerpi.php?action=setsocket&socket=SofaLed&status=1'))
---
+++ neon_off.rb
@@ -0,0 +1,4 @@
+#  http://192.168.2.101/lib/powerpi.php?action=setsocket&socket=Neonroehre&status=0
+require 'net/http'
+# Use Net::HTTP to fetch some html
+Net::HTTP.get(URI.parse('http://192.168.2.101/lib/powerpi.php?action=setsocket&socket=Neonroehre&status=0'))
